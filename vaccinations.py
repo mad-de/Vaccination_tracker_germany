11ee82fc1bacd82a7538d3769b230652a731499e
mad-de <cev_madde@msn.com>
2021-01-05 14:42:38 +0100

    Update vaccinations.py
    Fix broken RKI table content

--- vaccinations.py
+++ vaccinations.py
@@ -17,14 +17,17 @@ vaccinations_date = pd.read_excel('Impfquotenmonitoring.xlsx', sheet_name=0)
 
 vaccination_ger_num = vaccinations_table.values.tolist()[16][1]
 vaccination_ger_perc = vaccination_ger_num / 83002000 * 100
-vaccination_ger_perc = " (" + str("{:.2f}".format(vaccination_ger_perc)) + " %)"
 vaccination_ger_diff = vaccinations_table.values.tolist()[16][2]
-vaccination_date = vaccinations_date.values.tolist()[1][1]
-vaccination_date_time = vaccinations_date.values.tolist()[1][2]
-vaccination_date = vaccination_date.strftime("%m.%d.%Y")
+vaccination_date = str(vaccinations_date.values.tolist()[1])
+vaccination_date = vaccination_date.split('Impfungen_bis_einschl_', 1)[1]
+vaccination_date = vaccination_date.split(')', 1)[0]
+
+# Old format (until 04-01-2021)
+#vaccination_date_time = vaccinations_date.values.tolist()[1][2]
+#vaccination_date = vaccination_date.strftime("%m.%d.%Y")
 
 
-vaccinations = "Aktuelle COVID-19-Impfungen in Deutschland: " + str("{:.0f}".format(vaccination_ger_num)) + vaccination_ger_perc + ". Stand: " + str(format(vaccination_date)) + " um " + str(format(vaccination_date_time)) + ". " + str("{:.0f}".format(vaccination_ger_diff)) + " Impfungen wurden am Vortag durchgeführt."
+vaccinations = "Aktuelle COVID-19-Impfungen in Deutschland: Es wurden " + str("{:,.0f}".format(vaccination_ger_num)) + " Personen (" + str("{:.2f}".format(vaccination_ger_perc)) + " % der Bevölkerung) bis zum " + str(format(vaccination_date)) + "geimpft. " + str("{:,.0f}".format(vaccination_ger_diff)) + " Impfungen wurden am Vortag durchgeführt."
 
 
 print(vaccinations)
