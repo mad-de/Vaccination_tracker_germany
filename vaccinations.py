fa02bc666aeecf2ac99b499b0d5817c2efc0d196
mad-de <cev_madde@msn.com>
2021-01-11 13:26:53 +0100

    fix percentage calculation, update wording as now vaccinations =! vaccinated people as second doses are beginning to get administered

--- vaccinations.py
+++ vaccinations.py
@@ -26,19 +26,20 @@ vaccination_date = vaccination_date.split(')', 1)[0]
 
 vaccinations_diff_table = pd.read_excel('Impfquotenmonitoring.xlsx', sheet_name="Impfungen_proTag").fillna("missing").values.tolist()
 vaccinations_diff_dates = len(vaccinations_diff_table)
+vaccination_last_day = vaccinations_table.values.tolist()[16][3]
 
 # fix random appearing numbers in the table
 repl_string = 'Impfungen gesamt'
 while vaccinations_diff_table[vaccinations_diff_dates-1][0] == 'missing' or vaccinations_diff_table[vaccinations_diff_dates-1][0] == repl_string:
    vaccinations_diff_dates = vaccinations_diff_dates - 1
 
-vaccinations_diff = (float(vaccinations_diff_table[vaccinations_diff_dates-1][1]) - float(vaccinations_diff_table[vaccinations_diff_dates-2][1])) / float(vaccinations_diff_table[vaccinations_diff_dates-1][1]) * 100
+vaccinations_diff = (float(vaccination_last_day) - float(vaccinations_diff_table[vaccinations_diff_dates-2][1])) / float(vaccinations_diff_table[vaccinations_diff_dates-2][1]) * 100
 
 vaccinations_diff_vorz = ""
 if vaccinations_diff > 0:
    vaccinations_diff_vorz = "+"
 
-vaccinations = "Aktuelle COVID-19-Impfungen in Deutschland: Es wurden " + str("{:,.0f}".format(vaccination_ger_num)) + " Personen (" + str("{:.2f}".format(vaccination_ger_perc)) + " % der Bevölkerung) bis zum " + str(format(vaccination_date)) + " geimpft. An diesem Tag wurden " + str("{:,.0f}".format(vaccinations_diff_table[vaccinations_diff_dates-2][1])) + " Personen geimpft (" + vaccinations_diff_vorz + str("{:.2f}".format(vaccinations_diff)) + " % im Vergleich zum Vortag)."
+vaccinations = "Aktuelle COVID-19-Impfungen in Deutschland: Es wurden " + str("{:,.0f}".format(vaccination_ger_num)) + " Impfdosen (" + str("{:.2f}".format(vaccination_ger_perc)) + " Impfungen pro 100 Einwohner in Deutschland) bis zum " + str(format(vaccination_date)) + " durchgeführt. An diesem Tag wurden " + str("{:,.0f}".format(vaccination_last_day)) + " Personen geimpft (" + vaccinations_diff_vorz + str("{:.2f}".format(vaccinations_diff)) + " % im Vergleich zum Vortag)."
 
 if os.path.exists(datapath):
   os.remove(datapath)
